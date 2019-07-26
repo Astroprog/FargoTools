edfb29eab45f9b81f61acbc3763e1514f9a14ff0
Peter Rodenkirch <p.rodenkirch@gmail.com>
2019-07-26 11:36:07 +0200

    using setuptools

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import os
+from distutils.core import setup
+
+setup(  name="FargoTools"
+        ,version="alpha 1"
+        ,description="Toolset for Fargo simulation data"
+        ,author="Peter Rodenkirch"
+        ,packages=["FargoTools"]
+        )
